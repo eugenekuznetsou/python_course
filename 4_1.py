34598e9b06ada902ec58d556b8ba7614e46e8d0b
eugenekuznetsou <eugene.kuznetsou@gmail.com>
2023-02-14 09:28:48 +0300

    HW_4

--- 4_1.py
+++ 4_1.py
@@ -6,16 +6,3 @@ k = int(input('k: '))
 some_list = [i for i in range(k, 1000000) if i % m == 0]
 print(some_list[1:n + 1])
 
-
-# i = 1
-# while i <= n:
-#     if i % m == 0 and i > k:
-#         print(i)
-#     i += 1
-
-# i = 1
-# while i < n:
-#     for x in range(k,1000):
-#         if x % m == 0:
-#             print(x)
-#     i += 1
